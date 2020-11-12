81772e8c02c0f9980516e83b5a6201ac274903fa
Ankurkumarsaini <55224262+Ankurkumarsaini@users.noreply.github.com>
2020-11-12 16:18:27 +0530

    Update demo.js

--- routes/demo.js
+++ routes/demo.js
@@ -7,4 +7,13 @@ var router = express.Router();
 router.get('/', function (req, res, next) {    
     res.send('Router is come here!');
 });
+
+router.post('/',(req, res,next) => {
+    
+    console.log('franklin hit');
+    var intentName = req.body.queryResult.intent.displayName;
+    console.log(req.body);
+    console.log(intentName);
+});
+
 module.exports = router;
