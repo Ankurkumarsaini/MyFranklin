5027573c5f35a79dbe0162392b373b6058515018
Ankurkumarsaini <55224262+Ankurkumarsaini@users.noreply.github.com>
2020-11-12 17:22:27 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.render('index', { title: 'Express' });
-  //res.send('GET /Register');  
+  res.send('GET /Register');  
 });
 
 module.exports = router;
