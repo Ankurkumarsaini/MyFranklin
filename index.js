4f9820891932a412c7fcf5e43f8110a76b354bb4
Ankurkumarsaini <55224262+Ankurkumarsaini@users.noreply.github.com>
2020-11-12 16:44:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ var demoRouter = require('./routes/demo');
 const app = express()
 const port = process.env.PORT || 8200
 
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
