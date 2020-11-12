be414dde4a63967ab020aac6662acd6b21e8c272
Ankurkumarsaini <55224262+Ankurkumarsaini@users.noreply.github.com>
2020-11-12 16:26:44 +0530

    Update demo.js

--- routes/demo.js
+++ routes/demo.js
@@ -8,12 +8,60 @@ router.get('/', function (req, res, next) {
     res.send('Router is come here!');
 });
 
-router.post('/',(req, res,next) => {
-    
-    console.log('franklin hit');
+router.post('/',(req, res,next) => {    
+   
     var intentName = req.body.queryResult.intent.displayName;
-    console.log(req.body);
+  
     console.log(intentName);
+    try {
+		  switch (intentName) {			
+			case "BuzzWord":
+                // corporate buzz word generator
+                buzzWordHandler(req, res, next);
+                break;	           			
+			default:
+                logError("Unable to match intent. Received: " + intentName, req.body.originalDetectIntentRequest.payload.data.event.user, 'UNKNOWN', 'IDEA POST CALL');
+                res.send("Your request wasn't found and has been logged. Thank you!");
+                break;
+		  }
+	} catch (err) {
+        console.log(err);
+        res.send(err);
+    }
 });
+
+function buzzWordHandler(req, res, next) {	
+	https.get(
+		'https://corporatebs-generator.sameerkumar.website/',
+		responseFromAPI => {
+			let completeResponse = ''
+			responseFromAPI.on('data', chunk => {
+				completeResponse += chunk
+			})
+			responseFromAPI.on('end', () => {
+				
+				console.log(completeResponse);
+				
+				//const mymath = JSON.parse(completeResponse.text);
+				
+				const msg = JSON.parse(completeResponse);
+
+				let dataToSend ;
+				dataToSend = `Cool Corporate Buzz Word: ${msg.phrase}`
+				return res.json({
+					fulfillmentText: dataToSend,
+					source: 'BuzzWord'
+				})
+			})
+		},
+		error => {
+			return res.json({
+				fulfillmentText: 'Could not get results at this time',
+				source: 'BuzzWord'
+			})
+		}
+	)
+		
+}
 
 module.exports = router;
