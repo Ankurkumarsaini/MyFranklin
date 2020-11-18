0c530ef708bdfc40e542c5a178b375a21a4aacdf
Ankurkumarsaini <55224262+Ankurkumarsaini@users.noreply.github.com>
2020-11-18 13:08:53 +0530

    Update Ideas.js file

--- routes/ideas.js
+++ routes/ideas.js
@@ -2,10 +2,68 @@ var express = require('express');
 var bodyParser = require('body-parser')
 const rp = require('request-promise');
 var router = express.Router();
+const http = require('http')
+const https = require('https')
 
 
 router.get('/', function (req, res, next) {    
     res.send('Successfully connected to ideas');
 });
 
+
+router.post('/', function (req, res, next) {
+ var intentName = req.body.intentname;
+    console.log(intentName);
+    try {
+		  switch (intentName) {			
+			case "BuzzWord":
+                // corporate buzz word generator
+                buzzWordHandler(req, res, next);
+                break;	           			
+			default:
+                logError("Unable to match intent. Received: " + intentName, req.body.originalDetectIntentRequest.payload.data.event.user, 'UNKNOWN', 'IDEA POST CALL');
+                res.send("Your request wasn't found and has been logged. Thank you!");
+                break;
+		  }
+	} catch (err) {
+        console.log(err);
+        res.send(err);
+    }
+}
+     
+/*** buzzword handler function ***/
+function buzzWordHandler(req, res, next) {	
+	https.get(
+		'https://corporatebs-generator.sameerkumar.website/',
+		responseFromAPI => {
+			let completeResponse = ''
+			responseFromAPI.on('data', chunk => {
+				completeResponse += chunk
+			})
+			responseFromAPI.on('end', () => {
+				
+				console.log(completeResponse);
+				
+				//const mymath = JSON.parse(completeResponse.text);
+				
+				const msg = JSON.parse(completeResponse);
+
+				let dataToSend ;
+				dataToSend = `Cool Corporate Buzz Word: ${msg.phrase}`
+				return res.json({
+					fulfillmentText: dataToSend,
+					source: 'BuzzWord'
+				})
+			})
+		},
+		error => {
+			return res.json({
+				fulfillmentText: 'Could not get results at this time',
+				source: 'BuzzWord'
+			})
+		}
+	)
+		
+}
+            
 module.exports = router;
