d175fa3f23f2d9cd6ecaf612463a1292aed85252
Ankurkumarsaini <55224262+Ankurkumarsaini@users.noreply.github.com>
2020-11-19 15:27:23 +0530

    Update ideas.js

--- routes/ideas.js
+++ routes/ideas.js
@@ -122,19 +122,61 @@ function addNewIdeaWithName(req, res, next) {
 
                 }]
             }
-/** sending  data to the slack bot 
-	axios.post('https://hooks.slack.com/services/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',data)		
-	.then(res => {
-	    console.log(`statusCode: ${res.statusCode}`)
-	    console.log(res)
-	  })
-	  .catch(error => {
-	    console.error(error)
-	  })
-	  **/
-const response=axios.post('https://hooks.slack.com/services/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',data);
-console.log(res.data);
-	
+	const yourWebHookURL='https://hooks.slack.com/services/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';
+
+	console.log('Sending slack message');
+  try {
+    const slackResponse = sendSlackMessage(yourWebHookURL, data);
+    console.log('Message response', slackResponse);
+  } catch (e) {
+    console.error('There was a error with the request', e);
+  }
+
+}
+
+/*** slack Message Functions ***/
+function sendSlackMessage (webhookURL, messageBody) {
+  // make sure the incoming message body can be parsed into valid JSON
+  try {
+    messageBody = JSON.stringify(messageBody);
+  } catch (e) {
+    throw new Error('Failed to stringify messageBody', e);
+  }
+
+  // Promisify the https.request
+  return new Promise((resolve, reject) => {
+    // general request options, we defined that it's a POST request and content is JSON
+    const requestOptions = {
+      method: 'POST',
+      header: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    // actual request
+    const req = https.request(webhookURL, requestOptions, (res) => {
+      let response = '';
+
+
+      res.on('data', (d) => {
+        response += d;
+      });
+
+      // response finished, resolve the promise with data
+      res.on('end', () => {
+        resolve(response);
+      })
+    });
+
+    // there was an error, reject the promise
+    req.on('error', (e) => {
+      reject(e);
+    });
+
+    // send our message body (was parsed to JSON beforehand)
+    req.write(messageBody);
+    req.end();
+  });
 }
 
 
