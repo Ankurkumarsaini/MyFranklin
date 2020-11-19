b53380c8f763ecabf07a3a13d8ff0e70fde15ff1
Ankurkumarsaini <55224262+Ankurkumarsaini@users.noreply.github.com>
2020-11-19 14:54:42 +0530

    Update ideas.js

--- routes/ideas.js
+++ routes/ideas.js
@@ -123,7 +123,7 @@ function addNewIdeaWithName(req, res, next) {
                 }]
             }
 /** sending  data to the slack bot ***/
-	axios.post('https://hooks.slack.com/services/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',data)		
+	axios.post('https://hooks.slack.com/services/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',data)		
 	.then(res => {
 	    console.log(`statusCode: ${res.statusCode}`)
 	    console.log(res)
